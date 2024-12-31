0c1e6b630fae2ea086b206b2654e92d2f3308718
eliuttth-dev <eliuttth@gmail.com>
2024-12-31 19:20:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState }from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+
+
+const App = () => {
+    
+    const [menuIsActive, setMenuIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        console.log(`Is Open: ${menuIsActive}`)
+        setMenuIsActive(!menuIsActive);
+    }
+
+    return (
+        <div className="container">
+            <Header menuIsActive={menuIsActive} toggleMenu={toggleMenu}/>
+            {menuIsActive ? <></> : <main>Content</main>}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <nav>
+                <ul className="footer-language">
+                    <li>DE</li>
+                    <li>EN</li>
+                </ul>
+                <ul>
+                    <li>Payment & Shipping</li>
+                    <li>Contact</li>
+                    <li>Cancellation</li>
+                    <li>Terms & Conditions</li>
+                    <li>Privacy</li>
+                    <li>Imprint</li>
+                </ul>
+                <ul>
+                    <li>Instagram</li>
+                    <li>Facebook</li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer
