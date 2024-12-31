fc27744e5b19a7bc75de99d174d567af8db78b00
eliuttth-dev <eliuttth@gmail.com>
2024-12-31 19:26:02 +0100

    Formatter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-
 const App = () => {
-    
-    const [menuIsActive, setMenuIsActive] = useState(false);
+  const [menuIsActive, setMenuIsActive] = useState(false);
 
-    const toggleMenu = () => {
-        console.log(`Is Open: ${menuIsActive}`)
-        setMenuIsActive(!menuIsActive);
-    }
+  const toggleMenu = () => {
+    console.log(`Is Open: ${menuIsActive}`);
+    setMenuIsActive(!menuIsActive);
+  };
 
-    return (
-        <div className="container">
-            <Header menuIsActive={menuIsActive} toggleMenu={toggleMenu}/>
-            {menuIsActive ? <></> : <main>Content</main>}
-            <Footer />
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <Header menuIsActive={menuIsActive} toggleMenu={toggleMenu} />
+      {menuIsActive ? <></> : <main>Content</main>}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,48 +1,47 @@
 import React from "react";
-import { IconContext } from "react-icons"
+import { IconContext } from "react-icons";
 import { IoMdMenu } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
 const Header = ({ menuIsActive, toggleMenu }) => {
-    return(
-        <>
-            <header>
-                <h1>ENVY</h1>
-                { /* Mobile Navigation */}
-                <button onClick={toggleMenu}>
-                    <IconContext.Provider value={{size: 42}}>
-                        {menuIsActive ? <IoMdClose />: <IoMdMenu/> }
-                    </IconContext.Provider>
-                </button >
-            </header>
-            {menuIsActive && (
-                <nav>
-                    <ul>
-                        <li>Shop</li>
-                        <li>Collections</li>
-                        <li>Lookbook</li>
-                        <li>News</li>
-                        <li>About</li>
-                    </ul>
-                    {/* Show only in Desktop*/}
-                    <div className="nav-aside">
-                        <ul>
-                            <li>My Account</li>
-                            <li>Customer Service</li>
-                            <li>Currency / {/*This should change dinamically*/} $</li>
-                        </ul>
-                        {/* Suscribe */}
-                        <div>
-                            <p className="suscribe-title">10% OFF YOUR FIRST ORDER</p>
-                            <p className="suscribe-desc">ENVY-CLUB suscribes receive 10% off their first order <br/> 
-                            and early access to new drops. <span>Privacy Policy</span></p>
-                            <button>Suscribe</button>
-                        </div>
-                    </div>
-                </nav>
-            )}
-        </>
-
-    )
-}
+  return (
+    <>
+      <header>
+        <h1>ENVY</h1>
+        {/* Mobile Navigation */}
+        <button onClick={toggleMenu}>
+          <IconContext.Provider value={{ size: 42 }}>{menuIsActive ? <IoMdClose /> : <IoMdMenu />}</IconContext.Provider>
+        </button>
+      </header>
+      {menuIsActive && (
+        <nav>
+          <ul>
+            <li>Shop</li>
+            <li>Collections</li>
+            <li>Lookbook</li>
+            <li>News</li>
+            <li>About</li>
+          </ul>
+          {/* Show only in Desktop*/}
+          <div className="nav-aside">
+            <ul>
+              <li>My Account</li>
+              <li>Customer Service</li>
+              <li>Currency / {/*This should change dinamically*/} $</li>
+            </ul>
+            {/* Suscribe */}
+            <div>
+              <p className="suscribe-title">10% OFF YOUR FIRST ORDER</p>
+              <p className="suscribe-desc">
+                ENVY-CLUB suscribes receive 10% off their first order <br />
+                and early access to new drops. <span>Privacy Policy</span>
+              </p>
+              <button>Suscribe</button>
+            </div>
+          </div>
+        </nav>
+      )}
+    </>
+  );
+};
 
 export default Header;
